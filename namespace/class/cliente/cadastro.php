17400bf64d350336c4cb68849d996ceff67eb0b5
Gabriel <gabrielrenhardtdosreis@gmail.com>
2022-01-15 00:10:27 -0300

    aprendi a usar o namespace que é a forma que se utiliza o e se organiza as classes feita a partir da abstração e respetando as interfaces

---
+++ namespace/class/cliente/cadastro.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace cliente;
+
+class Cadastro extends \Cadastro {
+    
+    public function registrarVenda(){
+
+        echo "foi registrada uma venda, para o cliente " .$this->getNome();
+
+    }
+
+}
+
+?>
